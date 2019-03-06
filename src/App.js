347d2c03f2871a845e2fa5e20a41324235e67dbf
Angelicaalonzo <31522129+Angelicaalonzo@users.noreply.github.com>
2019-03-06 14:09:16 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl'; 
 import './App.css';
 
 class App extends Component {
